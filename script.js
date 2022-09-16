eb2d7d5f4f00b9b92ea3bb7b0d0cb45b7eac0375
Shweta Mishra <shmis2004@gmail.com>
2022-09-16 17:38:41 +0530

    final move

--- script.js
+++ script.js
@@ -4,13 +4,24 @@ function calcAge(birthYear) {
   const age = 2022 - birthYear;
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
+    let output = `You are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1995 && birthYear <= 2010) {
+      var genZ = true;
+      //Creating NEW variable with same name as outer scope's variable
+      const firstName = `Larry`;
+
+      //Reassigning outer scope's variable
+      output = "New Output";
       const str = `you are genZ , ${firstName}`;
       console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
     }
+    console.log(output);
   }
   printAge();
 
@@ -20,3 +31,31 @@ function calcAge(birthYear) {
 const firstName = "Shweta"; //global variable
 calcAge(2004);
 //console.log(age); not define in global scope
+
+// console.log(me);
+// console.log(job); //this will give error bcause it is in tdz
+// console.log(year);
+
+var me = "Shweta";
+let job = "teacher";
+const year = 1991;
+
+//function
+console.log(addDecl(2, 3));
+//console.log(addExp(2, 3)); const is in tdz (temporal dead)
+//console.log(addArrow(2, 3)); //arrow fnction variable is in tdz
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExp = function (a, b) {
+  return a + b;
+};
+
+//var addArrow = (a, b) => a + b;
+const addArrow = (a, b) => a + b;
+
+var x = 12;
+let y = 3;
+const z = 3;
